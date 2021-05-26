1949365c87fffe6e9ca9a38fdc68eabdd36d8660
chavalk <chavalk@hotmail.com>
2021-05-26 00:26:39 -0500

    Delete unused files from starter code.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const fileRoutes = require('./routes/file-upload');
 const userRoutes = require('./routes/user-routes');
 const imageRoutes = require('./routes/image-upload');
 
@@ -15,7 +14,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // app.use(require('./routes'));
-// app.use('/api/', fileRoutes);
 app.use('/api/', userRoutes);
 app.use('/api/', imageRoutes);
 
@@ -23,30 +21,3 @@ app.use('/api/', imageRoutes);
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
-
-
-// const express = require('express');
-// const path = require('path');
-// // const config = require('../config/config');
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // API routes
-// require('./routes')(app);
-
-// // Serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//   });
-// }
-
-// app.listen(PORT, () => {
-//   console.log(`API server running on port ${PORT}!`);
-// });
